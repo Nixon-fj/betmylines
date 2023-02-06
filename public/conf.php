7ae59e702a31a9f7ce61b739e2297aaecbeda494
Nixon-fj <71091247+Nixon-fj@users.noreply.github.com>
2023-02-06 11:07:23 -0600

    actualizacion 02/06/23

---
+++ public/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+define("SITE_NAME","Bet My Line");
+define("DOMAIN_NAME","betmylines.com");
+define("DGS_SITEID","361");
+
+$mobileEnabled = true;
+require "C:\\inetpub\\wwwroot\\generalConfig.php";
+?>
